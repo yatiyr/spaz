d6b1c0fd95e93d72efd126d077064ade05a3ceb4
BURAK EREN DERE <eren.dere@rsnet2.com.tr>
2020-08-05 14:29:48 +0300

    initial commit.

---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Tarayıcı penceresini oluştur.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  // DevTools'u aç.
+  win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Bazı API'ler sadece bu olayın gerçekleşmesinin ardından kullanılabilir.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // MacOS'de kullanıcı CMD + Q ile çıkana dek uygulamaların ve menü barlarının
+  // aktif kalmaya devam etmesi normaldir.
+  if (process.platform !== 'darwin') {
+    app.quit()
+    
+  }
+})
+
+app.on('activate', () => {
+  // MacOS'de dock'a tıklandıktan sonra eğer başka pencere yoksa
+  // yeni pencere açılması normaldir.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. Ayrıca bunları ayrı dosyalara koyabilir ve buradan isteyebilirsiniz.
