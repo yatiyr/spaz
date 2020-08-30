d0a3e6c912d686adec6ab2ebe028e0ba96bc02e9
yatiyr <dereeren@yahoo.com>
2020-08-30 13:14:58 +0300

    zoom action finished.

--- src/renderer/components/main/eti/diagramManager/actions/ZoomAction.ts
+++ src/renderer/components/main/eti/diagramManager/actions/ZoomAction.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 /* eslint-disable no-empty */
 /* eslint-disable no-var */
 import { WheelEvent } from 'react';
@@ -7,7 +8,7 @@ import {Action, ActionEvent, InputType, Mapping} from '@projectstorm/react-canva
 
 export default class ZoomAction extends Action {
 
-    constructor(options = {}) {
+    constructor() {
         super({
             type: InputType.MOUSE_WHEEL,
             // @ts-ignore
@@ -25,20 +26,38 @@ export default class ZoomAction extends Action {
                 }
 
                 var zoomLevel = model.getZoomLevel();
-
-                console.log(event.deltaY);
                     
-                if(event.deltaY < 0 && zoomLevel < 300) {
+                if((event.deltaY < 0 && zoomLevel < 300)) {
                     zoom.value = zoom.value >= zoom.max ? zoom.max : zoom.value + zoom.step;
                     model.setZoomLevel(zoomLevel + zoom.value);
-                    this.engine.repaintCanvas();
-
                 }
                 else if(event.deltaY > 0 && zoomLevel > 50) {
                     zoom.value = zoom.value >= zoom.max ? zoom.max : zoom.value + zoom.step;
                     model.setZoomLevel(zoomLevel - zoom.value);
-                    this.engine.repaintCanvas();
                 }
+
+                var zoomFactor = zoomLevel/100;
+                const boundRect = event.currentTarget.getBoundingClientRect();
+                const clientWidth = boundRect.width;
+                const clientHeight = boundRect.height;
+
+                const clientX = event.clientX - boundRect.left;
+                const clientY = event.clientY - boundRect.top;
+
+                const horizontalFactor = (clientX - model.getOffsetX())/ (zoomFactor) / clientWidth;
+                const verticalFactor   = (clientY - model.getOffsetY())/ (zoomFactor) / clientHeight;
+
+                const newZoomLevel = model.getZoomLevel();
+                const newZoomFactor = newZoomLevel/100;
+                const widthDiff  = clientWidth * (newZoomFactor) - clientWidth * (zoomFactor);
+                const heightDiff = clientHeight * (newZoomFactor) - clientHeight * (zoomFactor);
+
+                model.setOffset(
+                    model.getOffsetX() - widthDiff * horizontalFactor,
+                    model.getOffsetY() - heightDiff * verticalFactor
+                );
+
+                this.engine.repaintCanvas();
 
                 model.getLayers().forEach(layer => layer.allowRepaint(true));
             },
